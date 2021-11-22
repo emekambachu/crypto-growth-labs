d9fdd29448dc087dafa0d65bb312ed3eb63b1136
Emeka Victor Mbachu <victor.c.mbachu@gmail.com>
2021-11-22 21:15:14 +0100

    update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -495,6 +495,20 @@
     }
 </script>
 
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/619bf8996bb0760a4943d24f/1fl4k67dl';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
+
 </body>
 
 </html>
